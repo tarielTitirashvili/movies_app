e9723ea2b55ca6262cb56de9e91faf6c6b97ef1b
tarielTitirashvili <77127150+tarielTitirashvili@users.noreply.github.com>
2021-10-19 10:52:35 +0400

    final code

--- movies_app/src/components/movies/viewmovie/overlayMovieInfo.jsx
+++ movies_app/src/components/movies/viewmovie/overlayMovieInfo.jsx
@@ -3,7 +3,7 @@ import css from './overlayMovieInfo.module.css'
 import ReactYoutube from '../../reactYoutube/reactYoutube'
 
 export default function OverlayMovieInfo(props) {
-  const [page, setPage] = useState(0)
+  let [page, setPage] = useState(0)
   function onClick() {
     props.setOverlay(false)
     props.setTrailersAC([])
@@ -17,10 +17,12 @@ export default function OverlayMovieInfo(props) {
   }
   return (
     <div className = {css.overlay} style = {{width:props.overlay?'100%':'0vh'}}>
-      <div onClick = {()=>onClick()} className={css.closebtn} >&times;</div>
+      <div onClick = {()=>onClick()} className={css.closebtn}>&times;</div>
         <div className={css.overlay_content}>
-        {/* <iframe className = {css.trailer} src={`https://www.youtube.com/embed/${validateTrailers(props.trailers)?props.trailers[page].key:''}`} title={validateTrailers(props.trailers)?props.trailers[page].name:''} frameBorder="0" allow="autoplay; encrypted-media;" allowFullScreen></iframe> */}
-        {validateTrailers(props.trailers)? <ReactYoutube id = {props.trailers[page].key}/>:<h1>not found</h1>}        
+          {/* <iframe className = {css.trailer} src={`https://www.youtube.com/embed/${validateTrailers(props.trailers)?props.trailers[page].key:''}`} title={validateTrailers(props.trailers)?props.trailers[page].name:''} frameBorder="0" allow="autoplay; encrypted-media;" allowFullScreen></iframe> */}
+          { page? <div onClick = {()=>setPage(page--)} className = {css.arrow_left}>&lArr;</div>:''}
+          {validateTrailers(props.trailers)? <ReactYoutube id = {props.trailers[page].key}/>:<h1>not found</h1>}
+          { (page+1) <props.trailers.length? <div onClick = {()=>setPage(page++)} className = {css.arrow_right}>&rArr;</div>:''}
         </div>
     </div>
   )
